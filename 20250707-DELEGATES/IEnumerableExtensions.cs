31580fe450313709020f9d72d736c2f54f91f86a
fpd.core20@gmail.com <fpd.core20@gmail.com>
2025-07-07 19:02:06 +0300

    Добавьте файлы проекта.

---
+++ 20250707-DELEGATES/IEnumerableExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace _20250707_DELEGATES
+{
+    public static class IEnumerableExtensions
+    {
+        public static T GetMax<T>(this IEnumerable<T> collection, Func<T, float> convertToNumber) where T : class
+        {
+            if (collection == null || !collection.Any() || convertToNumber == null)
+                throw new ArgumentNullException(nameof(collection));
+
+            T result = collection.First();
+            float max = convertToNumber(result);
+
+
+            foreach (var item in collection)
+            {
+                float numericValue = convertToNumber(item);
+                if (max < numericValue)
+                {
+                    max = numericValue;
+                    result = item;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ 20250707-DELEGATES/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _20250707_DELEGATES
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
